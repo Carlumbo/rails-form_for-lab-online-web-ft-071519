82a1413f574365351a43d4c4baaa0897e06c1963
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-01 21:23:45 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,7 +13,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new(student_parmas)
+    @student = Student.new(student_params)
     @student.save
     redirect_to student_path(@student)
   end
