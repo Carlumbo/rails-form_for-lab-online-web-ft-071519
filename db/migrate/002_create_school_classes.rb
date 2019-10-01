fdb450c265fafabe8bcbe508615f8bad54abff3f
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-01 21:03:01 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_school_classes.rb
+++ db/migrate/002_create_school_classes.rb
@@ -1,9 +1,10 @@
-class CreateStudentClasses < ActiveRecord::Migration
+class CreateSchoolClasses < ActiveRecord::Migration
   def change
-    create_table :student_classes do |t|
+    create_table :school_classes do |t|
       t.string :title
       t.integer :room_number
 
+      t.timestamps null: false
     end
   end
-end 
+end
