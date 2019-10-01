0faa052315c53fdedf3d87a77040aa53d27c20c9
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-01 21:16:45 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,18 +1,27 @@
 class StudentsController < ApplicationController
 
   def show
+    @students = Student.find(params[:id])
   end
 
   def new
+    @student = Student.new
   end
 
   def edit
+    @student = Student.find(params[:id])
   end
 
   def create
+    @student = Student.new(student_parmas}
+    @student.save 
+    redirect_to student_path(@student)
   end
 
   def update
+    @student = Student.find(params[:id])
+    @studnet.update(student_params)
+    redirect_to student_path(@student)
   end
 
 
