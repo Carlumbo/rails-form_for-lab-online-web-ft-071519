ef8c29605d7aa290a9140b0909a6022c16986b93
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-10-01 20:58:57 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,27 +1,4 @@
 class SchoolClassesController < ApplicaionController
 
-  def new
-    @school_class = SchoolClass.new
-  end
-
-  def create
-    @school_class = SchoolClass.new(school_class_params(:title,:room_number))
-    @school_class.save
-    redirect_to  school_class_path(@school_class)
-
-  end
-
-  def edit
-
-  end
-
-  def show
-
-  end
-
-  def update
-
-  end
-
 
 end
